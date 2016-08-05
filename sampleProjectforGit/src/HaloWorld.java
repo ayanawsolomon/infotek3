8c8a30086524abe01dc0c8f6db878e2bb23a0a74
ayana_000 <ayana_000@sol.SOL>
2016-08-05 00:52:29 -0400

    Initial project is done

---
+++ sampleProjectforGit/src/HaloWorld.java
@@ -0,0 +1,12 @@
+
+public class HaloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("halo world");
+		for(int i =10; i <=20; i++){
+			System.out.println(i);
+		}
+	}
+
+}
